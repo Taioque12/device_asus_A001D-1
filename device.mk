ac7d8d537742a3d3fe3650015a1c809f92855123
GuilianoS <guiliano-sfalcin@hotmail.com>
2021-03-27 13:13:17 -0300

    fix brightness

--- device.mk
+++ device.mk
@@ -260,7 +260,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.A001D$(LOCAL_PATH)
+    android.hardware.light@2.0-service.A001D
 
 # Media
 PRODUCT_COPY_FILES += \
